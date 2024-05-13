b841008c8a7b1f1029f491a33ab046a0b07eca21
Sepuri30110 <furious30110@gmail.com>
2024-05-13 20:15:28 +0530

    done

--- frontend/src/components/professors.js
+++ frontend/src/components/professors.js
@@ -10,44 +10,39 @@ function Professors() {
         if (!hasloggedin) navigate('/professorlogin');
         // console.log(prof)
     }, [navigate])
-    
-    
+
+
 
 
     return (
-        <div>
-            <h1>{prof[0].hod}</h1>
-
-              <h3>Professor Details:</h3>
-        <div class="table-responsive">
-          <table class="table table-striped table-bordered">
-            <tr>
-              <th scope="col" >Prof code</th>
-              <th scope="col" >Name</th>
-              <th scope="col" >Class teacher</th>
-              <th scope="col" >Subjects</th>
-              <th scope="col" >Attendence</th>
-            </tr>
-            { for x in prof }
-            <tr>
-              <th scope="row">{{ x.pid }}</th>
-              <td>{{ x.prof_code }}</td>
-              <td>{{ x.prof_name }}</td>
-              <td>{{x.isClass_teacher}}</td>
-              <td>{}</td>
-              <td></td>
-              { if x.isHOD}
-              <td><a href="#" class="btn btn-warning ">Remove profile</a></td>
-              { else }
-              <td><a href="#" class="btn btn-warning disabled">Remove profile</a></td>
-              {% endif %}
-            </tr>
-            {% endfor %}
-          </table>
-        </div>
-      
-    
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">Prof code</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Class teacher</th>
+                    <th scope="col">Subjects</th>
+                    <th scope="col">Attendance</th>
+                </tr>
+            </thead>
+            <tbody>
+                {prof.map((x) => (
+                    <tr key={x.pid}>
+                        <th scope="row">{x.pid}</th>
+                        <td>{x.prof_code}</td>
+                        <td>{x.prof_name}</td>
+                        <td>{x.isClass_teacher ? 'Yes' : 'No'}</td>
+                        <td>{x.subjects}</td>
+                        <td>{/* Attendance data here */}</td>
+                        <td>
+                            <a href="#" className={x.isHOD ? "btn btn-warning" : "btn btn-warning disabled"}>
+                                Remove profile
+                            </a>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
 
